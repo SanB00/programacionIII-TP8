9b998d55c3bbf5380fe1f3a402f37725d859b497
SantiB <151892199+SanB00@users.noreply.github.com>
2026-06-10 13:17:36 -0300

    fix ValidationSettings

--- Vistas/Principal.Master.cs
+++ Vistas/Principal.Master.cs
@@ -2,6 +2,7 @@
 
 namespace Vistas {
     public partial class Principal : System.Web.UI.MasterPage {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>")]
         protected void Page_Load(object sender, EventArgs e) {
             lnkAgregarSucursal.NavigateUrl = "~/AgregarSucursal.aspx";
             lnkListarSucursales.NavigateUrl = "~/ListarSucursales.aspx";
